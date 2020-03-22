72eca851d33378f210d85d1f2936d46a6658dfb3
Albert Lai <albert.lai@algotizer.com>
2020-03-22 09:27:24 +0800

    Fix and set schedule job

--- src/hk_passenger_io/main.clj
+++ src/hk_passenger_io/main.clj
@@ -1,14 +1,20 @@
 (ns hk-passenger-io.main
-  (:require [reaver :as r :refer [parse extract-from text attr chain]]
-            [org.httpkit.client :as http]
-            [org.httpkit.sni-client]
-            [promesa.core :as p]
-            [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojure.string :as string])
-  (:import (java.time LocalDate)
-           (java.time.format DateTimeFormatter)
-           (java.time.temporal ChronoUnit)))
+  (:require
+    [clojure.data.csv :as csv]
+    [clojure.java.io :as io]
+    [clojure.string :as string]
+    [org.httpkit.client :as http]
+    [org.httpkit.sni-client]
+    [promesa.core :as p]
+    [reaver :as r :refer [parse extract-from text attr chain]])
+  (:import
+    (java.time
+      LocalDate)
+    (java.time.format
+      DateTimeFormatter)
+    (java.time.temporal
+      ChronoUnit)))
+
 
 (def dir "data/")
 
@@ -23,11 +29,15 @@
 
 (.format date-formatter first-date)
 
-(defn parse-int [num-str]
+
+(defn parse-int
+  [num-str]
   (when num-str
     (Integer/parseInt (string/replace num-str "," ""))))
 
-(defn parse-html [body]
+
+(defn parse-html
+  [body]
   (extract-from
     (parse body)
     "table.table-stat tr[class!=txt_bold]:gt(4)"
@@ -43,7 +53,9 @@
     "td:eq(8)" (chain text parse-int)
     "td:eq(9)" (chain text parse-int)))
 
-(defn fetch-rows [date]
+
+(defn fetch-rows
+  [date]
   (prn 'fetch date)
   (let [url (str "https://www.immd.gov.hk/eng/stat_"
                  (.format date-formatter date)
@@ -59,10 +71,13 @@
               (fn [rows]
                 (map #(assoc % :date date) rows)))))
 
+
 (def map->vec
   (juxt :date :control_point :arrive_resident :arrive_mainland :arrive_others :arrive_total :departure_resident :departure_mainland :departure_others :departure_total))
 
-(defn fetch-all [^LocalDate from-date ^LocalDate to-date]
+
+(defn fetch-all
+  [^LocalDate from-date ^LocalDate to-date]
   (let [n (.between ChronoUnit/DAYS from-date to-date)
         xf (comp (map #(.minusDays to-date %))
                  (map fetch-rows))]
@@ -70,12 +85,16 @@
               (fn [all] (apply concat all))
               #(sort-by :date %))))
 
-(defn write-csv! [rows fname]
+
+(defn write-csv!
+  [rows fname]
   (with-open [writer (io/writer fname :append true)]
     (csv/write-csv writer
                    rows)))
 
-(defn get-last-record-date [fname]
+
+(defn get-last-record-date
+  [fname]
   (when (.exists (io/file fname))
     (with-open [rdr (io/reader (io/file fname))]
       (->> rdr
@@ -87,7 +106,9 @@
            (.parse parse-date-formatter)
            (LocalDate/from)))))
 
-(defn -main [& args]
+
+(defn -main
+  [& args]
   (let [fname (str dir filename (.getYear (LocalDate/now)) ".csv")]
     (-> (fetch-all (or (get-last-record-date fname)
                        first-date)
